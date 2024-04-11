417b62f5057d2c3fbd48e9cbe8a0994f0b9abeb6
diogopires17 <63168110+diogopires17@users.noreply.github.com>
2024-04-11 12:32:45 +0100

    login done

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from models import init_db
-from flask import Flask, render_template
+from models import init_db, authenticate_user
+from flask import Flask, flash, redirect, render_template, request, session
+
+
 
 app = Flask(__name__)
+app.secret_key = 'IDHASIHDAIDHASKPCLS'  
 
 
 init_db()
 
 @app.route('/')
 def home():
+
     return render_template('index.html')
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        if password != 'admin' and password != '1':
+            password = hash(password)
+
+        
+        result, msg, email, password, user_name = authenticate_user(email, password)
+        if not result:
+            flash(msg, category='error')
+            return render_template('login.html')
+
+        else:
+            session['user_email'] = email # Set the user_id in the session
+            session['user_password'] = password # Set the user_password in the session
+            session['user_name'] = user_name # Set the user_name in the session
+            return redirect('/')
+    else:
+        return render_template('login.html')
+
 
 @app.route('/eventos')
 def about():
@@ -22,5 +47,6 @@ def about():
 def perfil():
     return render_template('perfil.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- models.py
+++ models.py
@@ -15,4 +15,25 @@ def init_db():
     cursor.execute("""INSERT OR IGNORE INTO users VALUES (NULL,'admin@gmail.com', 'admin', 'admin')""")
     cursor.execute("""INSERT OR IGNORE INTO users VALUES (NULL,'user1@gmail.com', 'User1', '1' )""")
     cursor.execute("""INSERT OR IGNORE INTO users VALUES (NULL,'user2@gmail.com', 'User2', '2' )""")
+
+    connection.commit()
+
+#                               FUNÇÕES AUXILIARES
+
+def authenticate_user(email, password):
+    if '/' in email:
+        return False, "email can't contain character '/'"
+    connection = sqlite3.connect('data.db', check_same_thread=False)
+    cursor = connection.cursor()
     
+    try:
+        cursor.execute( "SELECT email, password, name FROM users WHERE email= ? AND password = ?", (email, password))
+        results = cursor.fetchall()
+        if len(results) == 0:
+            return False, "Incorrect email or password", None, None, None
+        
+        return True, "", results[0][0], results[0][1], results[0][2]
+    
+    except Exception as e:
+        print(e)
+        return False, "There was an error validating your input, please check your data and try again.", None, None, None
